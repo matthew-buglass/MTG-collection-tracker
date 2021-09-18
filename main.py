569e7410f807e5de09fecca200b359cf18192177
mab839 <mab839@usask.ca>
2021-09-18 10:43:29 -0600

    first draft of file writing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #  Author: Matthew Buglass
 #  Maintainer: Matthew Buglass
 #  Website: matthewbuglass.com
-#  Date: 9/18/21, 10:30 AM
+#  Date: 9/18/21, 10:43 AM
 import json
 
 import requests
@@ -21,6 +21,7 @@ def get_default_cards():
     if response.status_code == 200:
         print("successfully retrieved bulk data.")
         response_json = response.json()
+        date = response_json["updated_at"]
 
         card_response = requests.get(response_json["download_uri"])
 
@@ -28,19 +29,33 @@ def get_default_cards():
             print("successfully retrieved default cards.")
             card_json = card_response.json()
             print("{} cards found".format("{:,}".format(len(card_json))))
-            print(json.dumps(card_json[0:3], indent=4))
-
-            for c in card_json[0:3]:
-                card = Card(c)
-                card.print_string()
-
             card_response.close()
+
+            card_list = []
+
+            for c in card_json:
+                card_list.append(Card(c))
+
+            return card_list, date
         else:
             print("failed to retrieve default cards. Error code: " + str(card_response.status_code))
+            card_response.close()
 
         response.close()
     else:
         print("failed to retrieve bulk data. Error code: " + str(response.status_code))
+        response.close()
+
 
 if __name__ == '__main__':
-    get_default_cards()
+    cards, date = get_default_cards()
+    header = cards[0].get_csv_header()
+
+    with open("mtg_card_data_"+date+".csv", "w+") as f:
+        f.write(header + "\n")
+
+        for c in cards:
+            f.write(c.get_csv_line())
+
+        f.close()
+
